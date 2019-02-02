a4479c5712dc5a79f5b116314a1a3c01aba23a48
Neko Haruyuki <qqts7nbd@gmail.com>
2019-02-02 15:48:33 +0900

    シーン作成のcreate関数メソッドを作成

---
+++ Classes/Scene/SceneData.cpp
@@ -0,0 +1,37 @@
+//
+//  SceneData.cpp
+//  PuzzleRPG
+//
+//  Created by neko on 2019/02/02.
+//
+
+#include "SceneData.h"
+
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+
+Node* SceneData::loaded()
+{
+    if(buildAndAddRoot() == nullptr){
+        return nullptr;
+    }
+    return m_node;
+}
+
+Node* SceneData::buildAndAddRoot()
+{
+    if(m_node != nullptr){
+        return m_node;
+    }
+    if(m_layer == nullptr || m_fileName.empty()){
+        return nullptr;
+    }
+    m_node = CSLoader::createNode(m_fileName);
+    if(m_node == nullptr){
+        return nullptr;
+    }
+    m_layer->addChild(m_node);
+    
+    return m_node;
+}
--- Classes/Scene/Title/TitleScene.h
+++ Classes/Scene/Title/TitleScene.h
@@ -2,20 +2,19 @@
 #define __TITLE_SCENE_H__
 
 #include "cocos2d.h"
+#include "SceneData.h"
 
 class TestController;
 
-class TitleScene : public cocos2d::Layer
+class TitleScene :
+public SceneData,
+public cocos2d::Layer
 {
 public:
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
     
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();
-    
-    // implement the "static create()" method manually
-    CREATE_FUNC(TitleScene);
+    SCENE_CREATE_FUNC(TitleScene, "title/TitleScene.csb")
+    bool onCreate() override;
     
 private:
     
