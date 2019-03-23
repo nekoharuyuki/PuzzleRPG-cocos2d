24dc362ed875ab3d5ae9e6703dcc64b5ca49f8d5
Neko Haruyuki <qqts7nbd@gmail.com>
2019-03-23 16:35:39 +0900

    シナリオ用のシーンを作成

--- proj.android/app/jni/Application.mk
+++ proj.android/app/jni/Application.mk
@@ -4,6 +4,7 @@ APP_STL := c++_static
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
 APP_ABI := armeabi-v7a
+APP_PLATFORM := android-9
 
 ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DNDEBUG=1
